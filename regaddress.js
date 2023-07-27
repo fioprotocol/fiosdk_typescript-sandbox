dc3a2787aacb620c6a50359e2658299675bab857
Pawel <pawelmas@gmail.com>
2023-07-27 13:13:20 -0600

    Create regaddress.js

---
+++ regaddress.js
@@ -0,0 +1,44 @@
+const {FIOSDK} = require('@fioprotocol/fiosdk')
+fetch = require('node-fetch')
+const fetchJson = async (uri, opts = {}) => {
+  return fetch(uri, opts)
+}
+
+const apiNode = 'https://fiotestnet.blockpane.com/v1/' // See https://github.com/fioprotocol/fio.mainnet for complete list of API endpoints
+const privateKey = '5HpJPnQmpbnTxaJbTEcMDj51DSbTW6hzQ41h1L4XcxXW83AX9Vw' // Get from http://monitor.testnet.fioprotocol.io:3000/#createKey and then faucet to add tokens: http://monitor.testnet.fioprotocol.io:3000/#faucet
+const fioHandle = 'username12345@regtest' // Use regtest domain on Testnet
+
+const publicKey = FIOSDK.derivedPublicKey(privateKey).publicKey
+const account = FIOSDK.accountHash(publicKey).accountnm
+
+action = 'regaddress'
+contract = 'fio.address'
+actionData = 
+{
+  fio_address: fioHandle,
+  owner_fio_public_key: '',
+  max_fee: 10000000000000,
+  tpid: '',
+  actor: account
+}
+
+const regaddress = async () => {
+  user = new FIOSDK(
+    privateKey,
+    publicKey,
+    apiNode,
+    fetchJson
+  )
+  try {
+    const result = await user.genericAction('pushTransaction', {
+      action: action,
+      account: contract,
+      data: actionData
+    })
+    console.log('Result: ', result)
+  } catch (err) {
+    console.log('Error: ', err)
+  }
+}
+
+regaddress();
