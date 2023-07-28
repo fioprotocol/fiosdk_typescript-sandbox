1456c3b04b445ee40b34cf10099bee8b1a6df3f6
Pawel <pawelmas@gmail.com>
2023-07-28 14:39:50 -0600

    Create trnsloctoks.js

---
+++ trnsloctoks.js
@@ -0,0 +1,58 @@
+// Register FIO Handle Action
+var action = 'trnsloctoks'
+var contract = 'fio.token'
+
+const {FIOSDK} = require('@fioprotocol/fiosdk')
+var fetch = require('node-fetch')
+const fetchJson = async (uri, opts = {}) => {
+  return fetch(uri, opts)
+}
+
+// EDIT this section then type node trnsloctoks.js in Terminal to run
+// API endpoint, see https://github.com/fioprotocol/fio.mainnet for complete list
+const apiNode = 'https://fiotestnet.blockpane.com/v1/' 
+// FIO Chain Testnet private key. Get one from http://monitor.testnet.fioprotocol.io:3000/#createKey
+// and then add tokens via faucet: http://monitor.testnet.fioprotocol.io:3000/#faucet
+const privateKey = '5K9du1JcEVNyBZKU6Uc9bSihgUzmMvGUxPDRwyxVmo9Xuh2NNW3'
+const publicKey = FIOSDK.derivedPublicKey(privateKey).publicKey
+const account = FIOSDK.accountHash(publicKey).accountnm
+// Action parameters
+var actionData = 
+{
+  payee_public_key: 'FIO7vveGx3JA5FMsTVCUQgPtZUuVPj39ouYEMz385x1nADmj3PNUP',
+  can_vote: 0,
+  periods: [
+    {
+      "duration": 86400,
+      "amount": 1000000000
+    }
+  ]
+  amount: 1000000000,
+  can_vote
+  max_fee: 10000000000000,
+  tpid: '',
+  actor: account
+}
+
+// Push transaction
+const main = async () => {
+  user = new FIOSDK(
+    privateKey,
+    publicKey,
+    apiNode,
+    fetchJson
+  )
+  try {
+    const result = await user.genericAction('pushTransaction', {
+      action: action,
+      account: contract,
+      data: actionData
+    })
+    console.log('Result: ', result)
+  } catch (err) {
+    console.log('Error: ', err)
+    console.log('Fields: ', err.json.fields)
+  }
+}
+
+main();
