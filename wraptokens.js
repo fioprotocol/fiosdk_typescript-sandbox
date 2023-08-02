503f85078632326f8a5e0670f9ba1b69c245c715
Pawel <pawelmas@gmail.com>
2023-08-02 16:35:02 -0600

    Create wraptokens.js

---
+++ wraptokens.js
@@ -0,0 +1,52 @@
+// Wrap FIO Tokens Action
+var action = 'wraptokens'
+var contract = 'fio.oracle'
+
+const {FIOSDK} = require('@fioprotocol/fiosdk')
+var fetch = require('node-fetch')
+const fetchJson = async (uri, opts = {}) => {
+  return fetch(uri, opts)
+}
+
+// EDIT this section then type node wraptokens.js in Terminal to run
+// API endpoint, see https://github.com/fioprotocol/fio.mainnet for complete list
+const apiNode = 'https://fiotestnet.blockpane.com/v1/' 
+// FIO Chain Testnet private key. Get one from http://monitor.testnet.fioprotocol.io:3000/#createKey
+// and then add tokens via faucet: http://monitor.testnet.fioprotocol.io:3000/#faucet
+const privateKey = '5K9du1JcEVNyBZKU6Uc9bSihgUzmMvGUxPDRwyxVmo9Xuh2NNW3'
+const publicKey = FIOSDK.derivedPublicKey(privateKey).publicKey
+const account = FIOSDK.accountHash(publicKey).accountnm
+// Action parameters
+var actionData = 
+{
+  amount: 1000000000,
+  chain_code: 'ETH',
+  public_address: '0x37F75C15B77AED5C0A49825B0F6BCED9822B8C40',
+  max_oracle_fee: 100000000000000,
+  max_fee: 10000000000000,
+  tpid: '',
+  actor: account
+}
+
+// Push transaction
+const main = async () => {
+  user = new FIOSDK(
+    privateKey,
+    publicKey,
+    apiNode,
+    fetchJson
+  )
+  try {
+    const result = await user.genericAction('pushTransaction', {
+      action: action,
+      account: contract,
+      data: actionData
+    })
+    console.log('Result: ', result)
+  } catch (err) {
+    console.log('Error: ', err)
+    console.log('Fields: ', err.json.fields)
+  }
+}
+
+main();
