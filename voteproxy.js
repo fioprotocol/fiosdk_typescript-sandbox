65b116f2620fb0a3c39b5a6837a1d18494c27e57
Pawel <pawelmas@gmail.com>
2023-08-03 15:34:31 -0600

    Create voteproxy.js

---
+++ voteproxy.js
@@ -0,0 +1,49 @@
+// Proxy Vote Action
+var action = 'voteproxy'
+var contract = 'eosio'
+
+const {FIOSDK} = require('@fioprotocol/fiosdk')
+var fetch = require('node-fetch')
+const fetchJson = async (uri, opts = {}) => {
+  return fetch(uri, opts)
+}
+
+// EDIT this section then type node voteproxy.js in Terminal to run
+// API endpoint, see https://github.com/fioprotocol/fio.mainnet for complete list
+const apiNode = 'https://fiotestnet.blockpane.com/v1/' 
+// FIO Chain Testnet private key. Get one from http://monitor.testnet.fioprotocol.io:3000/#createKey
+// and then add tokens via faucet: http://monitor.testnet.fioprotocol.io:3000/#faucet
+const privateKey = '5K9du1JcEVNyBZKU6Uc9bSihgUzmMvGUxPDRwyxVmo9Xuh2NNW3'
+const publicKey = FIOSDK.derivedPublicKey(privateKey).publicKey
+const account = FIOSDK.accountHash(publicKey).accountnm
+// Action parameters
+var actionData = 
+{
+  proxy: 'smoketestproxy1@fiotestnet',
+  fio_address: 'user10006@regtest',
+  max_fee: 0,
+  actor: account
+}
+
+// Push transaction
+const main = async () => {
+  user = new FIOSDK(
+    privateKey,
+    publicKey,
+    apiNode,
+    fetchJson
+  )
+  try {
+    const result = await user.genericAction('pushTransaction', {
+      action: action,
+      account: contract,
+      data: actionData
+    })
+    console.log('Result: ', result)
+  } catch (err) {
+    console.log('Error: ', err)
+    console.log('Fields: ', err.json.fields)
+  }
+}
+
+main();
