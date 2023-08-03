644f7c936be97cb2992a1a5c5b3566f5cf2e6788
Pawel <pawelmas@gmail.com>
2023-08-03 15:47:25 -0600

    Create regproducer.js

---
+++ regproducer.js
@@ -0,0 +1,51 @@
+// Register Block Producer Action
+var action = 'regproducer'
+var contract = 'eosio'
+
+const {FIOSDK} = require('@fioprotocol/fiosdk')
+var fetch = require('node-fetch')
+const fetchJson = async (uri, opts = {}) => {
+  return fetch(uri, opts)
+}
+
+// EDIT this section then type node regproducer.js in Terminal to run
+// API endpoint, see https://github.com/fioprotocol/fio.mainnet for complete list
+const apiNode = 'https://fiotestnet.blockpane.com/v1/' 
+// FIO Chain Testnet private key. Get one from http://monitor.testnet.fioprotocol.io:3000/#createKey
+// and then add tokens via faucet: http://monitor.testnet.fioprotocol.io:3000/#faucet
+const privateKey = '5K9du1JcEVNyBZKU6Uc9bSihgUzmMvGUxPDRwyxVmo9Xuh2NNW3'
+const publicKey = FIOSDK.derivedPublicKey(privateKey).publicKey
+const account = FIOSDK.accountHash(publicKey).accountnm
+// Action parameters
+var actionData = 
+{
+  fio_address: 'user10006@regtest',
+  fio_pub_key: 'FIO7tHihSLJVRQ2HsiVGTXAh3RgfXQvVVPpMm8pwQtiTHQHPnY5dH',
+  url: 'https://bobp.io/',
+  location: 80,
+  max_fee: 100000000000,
+  actor: account
+}
+
+// Push transaction
+const main = async () => {
+  user = new FIOSDK(
+    privateKey,
+    publicKey,
+    apiNode,
+    fetchJson
+  )
+  try {
+    const result = await user.genericAction('pushTransaction', {
+      action: action,
+      account: contract,
+      data: actionData
+    })
+    console.log('Result: ', result)
+  } catch (err) {
+    console.log('Error: ', err)
+    console.log('Fields: ', err.json.fields)
+  }
+}
+
+main();
